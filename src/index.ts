437b25857604ac9508cb32f01631b72e650593e9
AnetaPy <aneta.pych15@gmail.com>
2024-04-22 19:03:53 +0200

    Added checking whether the Reaserce field has an asterisk

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-import * as data from "../data.json";
+import * as data from "./data.json";
+
+import {OneStatement, OneUser} from "./types";
+
+const allStatements: OneStatement[] = data.PolicyDocument.Statement;
+const allUsersArray: OneUser[] = [];
+
+for (const statement of allStatements) {
+    const {Sid, Resource} = statement;
+
+    if (Array.isArray(Resource)) {
+        allUsersArray.push({
+            name: Sid,
+            output: true,
+        });
+    } else {
+        allUsersArray.push({
+            name: Sid,
+            output: Resource.split("*").length - 1 !== 1,
+        });
+    }
+}
+console.log(allUsersArray);
